87c6f6b06f0637d996bf1f3cfa509640cbf1d963
Julius Jänchen <julius.jaenchen@check24.de>
2022-05-23 11:24:52 +0200

    reformatted streams

--- src/main/java/hwr/oop/riddler/model/component/CellGroup.java
+++ src/main/java/hwr/oop/riddler/model/component/CellGroup.java
@@ -6,9 +6,15 @@ import static java.util.stream.Collectors.toSet;
 
 public record CellGroup(Set<Cell> cells) {
     public Set<Cell> getUnsolvedCells() {
-        return cells.stream().filter(Cell::isEmpty).collect(toSet());
+        return cells.stream()
+                .filter(Cell::isEmpty)
+                .collect(toSet());
     }
+
     public Set<Integer> getCellValues() {
-        return cells.stream().filter(Cell::isFilled).map(Cell::getValue).collect(toSet());
+        return cells.stream()
+                .filter(Cell::isFilled)
+                .map(Cell::getValue)
+                .collect(toSet());
     }
 }
