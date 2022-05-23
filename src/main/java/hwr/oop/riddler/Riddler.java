24d1d3147853e557b27579b9a0c6f26f411d6be4
Julius Jänchen <julius.jaenchen@check24.de>
2022-05-23 10:25:40 +0200

    added static to main() again

--- src/main/java/hwr/oop/riddler/Riddler.java
+++ src/main/java/hwr/oop/riddler/Riddler.java
@@ -8,7 +8,7 @@ import hwr.oop.riddler.model.Sudoku;
 import java.io.File;
 
 public class Riddler {
-    public void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) throws IllegalArgumentException {
         var parser = new SudokuParser();
         var solver = new BacktrackingSolver();
 
@@ -21,7 +21,7 @@ public class Riddler {
         new SudokuPrinter(System.out).print(solvedSudoku);
     }
 
-    private String parseFilepathFromArgs(String[] args) {
+    private static String parseFilepathFromArgs(String[] args) {
         if (args.length != 1)
             throw new IllegalArgumentException("USAGE: java hwr.oop.riddler.Riddler [filepath]");
         return args[args.length - 1];
